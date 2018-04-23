e6f31bbbd2bb8f2673d55ef66b46ba88c01e06f3
niyohn <niyohn@stekligan.se>
2018-04-23 08:04:12 +0200

    fixed bug related to uninitialized memory in compiler, and fixed alot of minor bugs also added a debug macro that prints alot of info

--- compiler.c
+++ compiler.c
@@ -136,64 +136,107 @@ unsigned char translate_operator(char* operator, char* operand_a, char* operand_
 
   if(strcmp(operator, "stu") == 0) {
     out = 0xC0;
-    out = out | (translate_value(operand_a) << 2); 
+#ifdef debug
+    printf("0x%02X 0x%02X 0x%02X\n", out, translate_value(operand_a) << 4, translate_value(operand_b));
+#endif
+    out = out | (translate_value(operand_a) << 4);
     out = out | translate_value(operand_b);
   }
   else if(strcmp(operator, "stl") == 0) {
     out = 0x80;
-    out = out | (translate_value(operand_a) << 2); 
+#ifdef debug
+    printf("0x%02X 0x%02X 0x%02X\n", out, translate_value(operand_a) << 4, translate_value(operand_b));
+#endif
+    out = out | (translate_value(operand_a) << 4);
     out = out | translate_value(operand_b);
   }
   else if(strcmp(operator, "add") == 0) {
     out = 0x10;
+#ifdef debug
+    printf("0x%02X 0x%02X 0x%02X\n", out, translate_value(operand_a) << 2, translate_value(operand_b));
+#endif
     out = out | (translate_value(operand_a) << 2); 
     out = out | translate_value(operand_b); 
   }
   else if(strcmp(operator, "sub") == 0) {
     out = 0x20;
+#ifdef debug
+    printf("0x%02X 0x%02X 0x%02X\n", out, translate_value(operand_a) << 2, translate_value(operand_b));
+#endif
     out = out | (translate_value(operand_a) << 2); 
     out = out | translate_value(operand_b); 
   }
   else if(strcmp(operator, "mup") == 0) {
     out = 0x30;
+#ifdef debug
+    printf("0x%02X 0x%02X 0x%02X\n", out, translate_value(operand_a) << 2, translate_value(operand_b));
+#endif
     out = out | (translate_value(operand_a) << 2); 
     out = out | translate_value(operand_b); 
   }
   else if(strcmp(operator, "beq") == 0) {
     out = 0x40;
+#ifdef debug
+    printf("0x%02X 0x%02X 0x%02X\n", out, translate_value(operand_a) << 2, translate_value(operand_b));
+#endif
     out = out | (translate_value(operand_a) << 2); 
     out = out | translate_value(operand_b); 
   }
-  else if(strcmp(operator, "stl") == 0) {
+  else if(strcmp(operator, "slt") == 0) {
     out = 0x50;
+#ifdef debug
+    printf("0x%02X 0x%02X 0x%02X\n", out, translate_value(operand_a) << 2, translate_value(operand_b));
+#endif
     out = out | (translate_value(operand_a) << 2); 
     out = out | translate_value(operand_b); 
   }
   else if(strcmp(operator, "and") == 0) {
     out = 0x60;
+#ifdef debug
+    printf("0x%02X 0x%02X 0x%02X\n", out, translate_value(operand_a) << 2, translate_value(operand_b));
+#endif
     out = out | (translate_value(operand_a) << 2); 
     out = out | translate_value(operand_b); 
   }
   else if(strcmp(operator, "lor") == 0) {
     out = 0x70;
+#ifdef debug
+    printf("0x%02X 0x%02X 0x%02X\n", out, translate_value(operand_a) << 2, translate_value(operand_b));
+#endif
     out = out | (translate_value(operand_a) << 2); 
     out = out | translate_value(operand_b); 
   }
   else if(strcmp(operator, "out") == 0) {
     out = 0x00;
+#ifdef debug
+    printf("0x%02X 0x%02X\n", out, translate_value(operand_a));
+#endif
     out = out | translate_value(operand_a); 
   }
   else if(strcmp(operator, "pus") == 0) {
     out = 0x04;
+#ifdef debug
+    printf("0x%02X 0x%02X\n", out, translate_value(operand_a));
+#endif
     out = out | translate_value(operand_a); 
   }
   else if(strcmp(operator, "pop") == 0) {
     out = 0x08;
+#ifdef debug
+    printf("0x%02X 0x%02X\n", out, translate_value(operand_a));
+#endif
     out = out | translate_value(operand_a); 
   }
   else if(strcmp(operator, "jmp") == 0) {
     out = 0x0C;
+#ifdef debug
+    printf("0x%02X 0x%02X\n", out, translate_value(operand_a));
+#endif
   out = out | translate_value(operand_a); 
+  }
+  else {
+    fprintf(stderr, "unrecognized operator %s\n", operator);
+    exit(1);
   }
 
   return out;
@@ -218,16 +261,20 @@ int parse(unsigned char* ins, char* line) {
   char* ptr = line;
   int is_label = false;
   char* operator = malloc(LABEL_LENGTH * sizeof(char));
+  memset(operator, 0, LABEL_LENGTH);
   char* operand_a = malloc(LABEL_LENGTH * sizeof(char));
+  memset(operand_a, 0, LABEL_LENGTH);
   char* operand_b = malloc(LABEL_LENGTH * sizeof(char));
+  memset(operand_b, 0, LABEL_LENGTH);
   
   get_word(&operator, &ptr);
   
   get_word(&operand_a, &ptr);
   get_word(&operand_b, &ptr);
 
-
+#ifdef debug
   printf("%s %s %s\n", operator, operand_a, operand_b);
+#endif
   *ins = translate_operator(operator, operand_a, operand_b);
   
   free(operator);
